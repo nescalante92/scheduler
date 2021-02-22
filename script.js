89701f96c374724f1b92c11bdb6b2c5222c0370e
Nelson Escalante <nescalante92@gmail.com>
2021-02-22 00:35:03 -0500

    added date and time

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-var timeContainer = document.getElementsByClassName ("container");
+var currentDay = moment();
+
+$("#currentDay").text(currentDay.format("[Today is ]dddd: MMMM DD, YYYY h:mmA"));
+/*toString() returns string object value*/ 
+console.log(currentDay.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function moment() {
+    // currentDay = ("02/21/2021 9:09PM","MM/DD/YYYY, h:mmA");
+    // console.log(currentDay);
+    // }
+    // moment()
+    
+    
+    /*Today's date and time in Momment format*/ 
+    /*code cite: https://momentjs.com/*/ 
+    
+    // $("currentDay").text(today.format("MMM Do, YYYY"));
+    // moment().format('MMMM Do YYYY, h:mm:ss a');
+    
+    
+    //------------------------------------------------------
+    // $(window).load(function(){
+    //     // Initiate moment here and console.log it
+    //     console.log( moment() );
+    // });
+    //------------------------------------------------------
+    
+    
+    
+
+
+
+
+
+
+
